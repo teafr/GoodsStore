0ceae93f02309b30bf9d23e9e4cd63e8256454a4
teafr <tookyoumy7@gmail.com>
2025-09-03 10:49:17 +0300

    updated exception handling with help of new class which extends Error

--- API/middlewares/error.middleware.js
+++ API/middlewares/error.middleware.js
@@ -1,10 +1,7 @@
 function errorMiddleware(err, req, res, next) {
-    const status = err.status || 500;
-    const message = err.message || 'Internal Server Error';
-
-    res.status(status).json({
-        success: false,
-        error: message,
+    res.status(err.statusCode || 500).json({
+        status: err.status || 'error',
+        message: err.message || 'Internal Server Error',
         stack: process.env.NODE_ENV === 'production' ? undefined : err.stack,
     });
 }
