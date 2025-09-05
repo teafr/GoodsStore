b7c76078a05a66e584219675abbbcdfe0e09fd59
teafr <tookyoumy7@gmail.com>
2025-09-05 19:42:39 +0300

    Made product service for getting products from database

---
+++ UI/src/app/services/product.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../../environment/environment";
+import { HttpClient } from "@angular/common/http";
+import { Product } from "../models/product.model";
+import { Observable } from "rxjs";
+
+@Injectable({ providedIn: 'root' })
+export class ProductService {
+  private baseUrl = environment.apiUrl + '/products';
+
+  constructor(private http: HttpClient) {}
+
+  getProducts() : Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
+  }
+
+  getProductById(id: string) {
+    return this.http.get<Product>(`${this.baseUrl}/${id}`);
+  }
+}
