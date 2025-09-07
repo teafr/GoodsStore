4b03a3245839b6797f9019873744932b2c8c39cf
teafr <tookyoumy7@gmail.com>
2025-09-07 15:25:00 +0300

    made components for registration and login

--- UI/src/app/components/register/register.ts
+++ UI/src/app/components/register/register.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { passwordsMatchValidator } from '../../validators/validators';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
 export class Register {
+  form: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) { 
+    this.form = this.formBuilder.group({
+      lastName: ['', Validators.required],
+      firstName: ['', Validators.required],
+      patronymic: [''],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmationPassword: ['', Validators.required]
+    }, { validators: passwordsMatchValidator });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      const formData = this.form.value;
+      console.log('Form Data:', formData);
+      this.authService.register(formData.lastName, formData.firstName, formData.patronymic || '', formData.email, formData.password).subscribe(user => {        
+        if (user) {
+          console.log('Registration successful:', user);
+          this.router.navigateByUrl('/products');
+        }
+      });
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
