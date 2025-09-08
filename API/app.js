75ab52aa51436bf2803036c6ba6e8f1169d1b80e
teafr <tookyoumy7@gmail.com>
2025-09-09 01:34:09 +0300

    Made OpenAPI swagger documentation with help of YAML

--- API/app.js
+++ API/app.js
@@ -3,6 +3,10 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import YAML from 'yamljs';
+
 import productRoutes from './routes/product.routes.js';
 import saleRoutes from './routes/sale.routes.js';
 import authRoutes from './routes/auth.routes.js';
@@ -11,6 +15,7 @@ import errorMiddleware from './middlewares/error.middleware.js';
 import authMiddleware from './middlewares/auth.middleware.js';
 
 const app = express();
+const swaggerDocument = YAML.load('./docs/swagger.yaml');
 
 app.use(cookieParser());
 app.use(cors({
@@ -20,13 +25,12 @@ app.use(cors({
 app.use(json()); 
 app.use(morgan('dev'));
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use('/auth', authRoutes);
 app.use('/products', productRoutes);
-
 app.use(authMiddleware);
-
 app.use('/sales', saleRoutes);
-
 
 app.use(errorMiddleware);
 
